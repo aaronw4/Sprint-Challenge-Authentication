d8d965917a8f556028f67c645a0a058c9c523956
Aaron Williams <aaronrw4@yahoo.com>
2020-02-21 19:26:02 -0800

    Added testing code to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -13,5 +13,19 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 }
